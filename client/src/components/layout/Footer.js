5ed4f4c66e9b09ea379bee2c1d7bb5fefed32fed
pratikn1998 <pratiknallamotu@gmail.com>
2020-01-30 16:26:13 -0500

    footer style

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer-style'>
+
       <div className='footer-container text-center'>
+      <a href="https://www.linkedin.com/in/pratiknallamotu/"><i class="fab fa-linkedin mr-2"></i></a>
+      <a href="https://github.com/pratikn1998"><i class="fab fa-github mr-2"></i></a>
+
         <span className='text-center'>
           Icons made by{' '}
           <a
