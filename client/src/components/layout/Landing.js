62861f2455ae73dbcf4e7b80b2c92a3bc2f6c0d9
pratikn1998 <pratiknallamotu@gmail.com>
2020-01-30 05:41:10 -0500

    Landing page edits

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,13 +6,13 @@ const Navbar = () => {
   return (
     <section className={styles.landing}>
       <div className={styles.landingInner}>
-        <h1 className='x-large'>WELCOME!</h1>
-        <p className='lead'>
+        <h1 className={styles.x-large}>WELCOME!</h1>
+        <p className={styles.lead}>
           The purpose of this website to help you learn more about llamas and
           myself as a Front-End Developer!
         </p>
-        <div className='buttons'>
-          <Link to='/llamas' className='btn btn-primary'>
+        <div className={styles.buttons}>
+          <Link to='/llamas' className={'styles.btn styles.btnPrimary'}>
             About Llamas
           </Link>
           <Link to='/aboutpratik' className='btn btn-light'>
